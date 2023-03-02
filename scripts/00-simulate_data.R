f5a640ae33b976b9f2bd31c34f9dd98563c5c3ed
Sebastian Rodriguez <me@srod.ca>
2023-03-02 10:56:45 -0500

    Init simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,10 +10,29 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+simulated_data <-
+  tibble(
+    # Patient categories, predefined and set terms
+    "tea_number" = c(1:8),
+    
+    # Response time mandates, predefined and set numbers
+    "guess" = sample(
+      x = c(1:2),
+      size = 8,
+      replace = TRUE
+    ),
+    
+    # Performance mandates, predefined and set percentage
+    "actual" = sample(
+      x = c(1:2),
+      size = 8,
+      replace = TRUE
+    ),
+  )
+simulated_data
 
 
 
